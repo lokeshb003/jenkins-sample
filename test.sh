93279caa6725e7be9fbc896b05366d2cf4c009c0
Lokesh B <lokesh.b.2020.ad@ritchennai.edu.in>
2023-03-19 18:25:17 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Check if port 80 is open
-nc -zv localhost 80 > /dev/null 2>&1
+# Check if port 8089 is open
+nc -zv localhost 8089 > /dev/null 2>&1
 
 # Check the exit code of the previous command
 if [ $? -eq 0 ]; then
